cb80dbd2803d5831dcb581916b767cc6ad0b3484
Roman Bybyk <64772371+rom4keee@users.noreply.github.com>
2022-07-30 21:09:37 +0300

    Update ExperimentalPlane.java

--- aircompany-master/Java/src/main/java/Planes/ExperimentalPlane.java
+++ aircompany-master/Java/src/main/java/Planes/ExperimentalPlane.java
@@ -3,28 +3,37 @@ package Planes;
 import models.ClassificationLevel;
 import models.ExperimentalTypes;
 
-public class experimentalPlane extends Plane{
 public class ExperimentalPlane extends Plane {
 
-    private ExperimentalTypes type;
     private ExperimentalTypes experimentalTypes;
     private ClassificationLevel classificationLevel;
 
-    public experimentalPlane(String model, int maxSpeed, int maxFlightDistance, int maxLoadCapacity, ExperimentalTypes type, ClassificationLevel classificationLevel) {
     public ExperimentalPlane(String model, int maxSpeed, int maxFlightDistance, int maxLoadCapacity, ExperimentalTypes type, ClassificationLevel classificationLevel) {
         super(model, maxSpeed, maxFlightDistance, maxLoadCapacity);
-        this.type = type;
         this.experimentalTypes = type;
         this.classificationLevel = classificationLevel;
     }
 
-@Override
+    public ClassificationLevel getClassificationLevel(){
+        return classificationLevel;
+    }
+
+    public void setClassificationLevel(ClassificationLevel classificationLevel){
+        this.classificationLevel = classificationLevel;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        return super.equals(o);
+    }
+
+    @Override
     public int hashCode() {
+        return super.hashCode();
+    }
 
     @Override
     public String toString() {
-        return "experimentalPlane{" +
-                "model='" + model + '\'' +
         return "ExperimentalPlane{" +
                 "model='" + planeModel + '\'' +
                 '}';
