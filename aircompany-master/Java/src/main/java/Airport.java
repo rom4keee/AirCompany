604f30bfea3bfcbe2d763dce75e449dfd83d0acf
Roman Bybyk <64772371+rom4keee@users.noreply.github.com>
2022-07-31 22:15:51 +0300

    Update Airport.java

--- aircompany-master/Java/src/main/java/Airport.java
+++ aircompany-master/Java/src/main/java/Airport.java
@@ -6,14 +6,8 @@ import Planes.Plane;
 
 import java.util.*;
 
-// version: 1.1
-// made by Vitali Shulha
-// 4-Jan-2019
-
 public class Airport {
     private List<? extends Plane> planes;
-
-
 
     public List<PassengerPlane> getPassengerPlanes() {
         List<? extends Plane> l = this.planes;
